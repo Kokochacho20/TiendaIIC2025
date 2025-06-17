1ec461e91fc709351e9e5999e2e1aa1dff0b0541
Jose D <Jose D@JoseD-PC>
2025-06-16 21:22:05 -0600

    Avance semana 6

---
+++ src/main/java/com/tienda/domain/Producto.java
@@ -0,0 +1,37 @@
+package com.tienda.domain;
+
+import jakarta.persistence.*;
+import java.io.Serializable;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "Producto")
+public class Producto implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_Producto")
+    private Long idProducto;
+    @Column(name = "id_categoria")
+    private Long idCategoria;
+    private String descripcion;
+    private String detalle;
+    private double precio;
+    private int existencias;
+    @Column(name = "ruta_imagen")
+    private String rutaImagen;
+    private boolean activo;
+
+    public Producto() {
+
+    }
+
+    public Producto(String descripcion, boolean activo) {
+        this.descripcion = descripcion;
+        this.activo = activo;
+    }
+
+}
